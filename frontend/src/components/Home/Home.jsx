014d70cde56cb9c808e1c1fee0cade3b03b26889
Fabrizio <fabacasa@gmail.com>
2021-10-14 14:11:17 +0200

    pagination done - sorting next

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,39 +1,39 @@
 import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 
+import Pagination from "../Pagination/Pagination";
 import ProductList from "../ProductList/ProductList";
 import { productContext } from "../../context/productContext";
 
 const Home = () => {
   const { games, setGames } = useContext(productContext)
   const { search, setSearch } = useContext(productContext)
+
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
   useEffect(() => {
     (() => {
       try {
+        setLoading(true);
         axios.get(`http://localhost:5000/api/products/`)
           .then(response => {
-            setGames(games.concat(response.data))            
-            console.log(games);
+            setGames(games.concat(response.data))
+            setLoading(false);
+            // console.log(games);
           })
       } catch (e) {
         console.log(e);
       }
     })();
   }, []);
-
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     setLoading(true);
-  //     const res = await axios.get('http://localhost:3002/api/items/%27); 
-  //     setPosts(res.data);
-  //     console.log(res.data)
-  //     setLoading(false)
-  //   }
-  //   fetchPosts();
-  // }, []);
-
-
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentGames = games.slice(indexOfFirstPost, indexOfLastPost);
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
   // const handleChange = async (e) => {
   //   setSearch(e.target.value)
   //   await new Promise(resolve => setTimeout(resolve, 4000))
@@ -47,7 +47,8 @@ const Home = () => {
         <input type="text" name="game_maker" placeholder="Search by game or console!" />
         <input type="button" value="Search!" />
       </div>
-      <ProductList games={games} />
+      <ProductList currentGames={currentGames} loading={loading} />
+      <Pagination postsPerPage={postsPerPage} totalGames={games.length} paginate={paginate} />
     </section>
   )
 };
